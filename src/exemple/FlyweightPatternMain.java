4fb9f6ad6ff0d09493c71be15fe2c2cee8c34fab
veve <veve@DESKTOP-DU63UOI>
2018-02-26 13:27:46 +0100

    add  : 	-Fichier de test 		-Comparateur de résultat

--- src/exemple/FlyweightPatternMain.java
+++ src/exemple/FlyweightPatternMain.java
@@ -2,21 +2,58 @@ package exemple;
 
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import  exemple.Fichier;
 
 public class FlyweightPatternMain {
-    
- public static void main(String[] args) {
-          System.out.println(" LET IT GO ");
- // Instancie la fabrique
-          PoidsMoucheFactory lFlyweightFactory = new PoidsMoucheFactory();
+	
+	static PoidsMoucheFactory flyweightFactory = new PoidsMoucheFactory();
+
+	static List<IObjetSpatial> listLourde = new ArrayList<IObjetSpatial>();
+
+	public static void main(String[] args) {
+		System.out.println(" LET IT GO ");
+		// Instancie la fabrique
+		testByFile("src\\resources\\OnOrbitByNorad.csv");
+		testByFile("src\\resources\\NotOnOrbitByNorad.csv");
+		testByFile("src\\resources\\ObjSpatial.csv");
+		
+
+	}
+	
+	public static void testByFile(String filePath) {
+		listLourde.clear();
+		//InitFichier
+				final File file = Fichier.getResource(filePath);
+				List<String> lines = Fichier.readFile(file);
+
+				
+				long startTime = System.nanoTime();
+				// Here is the code to measure
+				for (String obj : lines) {
+					flyweightFactory.getObjetSpatial(obj.split(",")[2]);
+				}
+				// stop stopwatch
+				long endTime = System.nanoTime();
+				
+				
+				System.out.println("Méthode avec FleyWeight : " + (endTime - startTime) + " ns");	
 
 
-//InitFichier
-    final File file = Fichier.getResource("src\\resources\\OnOrbitByNorad.csv");
-    List<String> lines = Fichier.readFile(file);
-    
-     System.out.println(" list : "+lines.size());
- }
+				startTime = System.nanoTime();
+				// Here is the code to measure
+				for (String objBis : lines) {
+					listLourde.add(new ObjetSpacialPartage(objBis.split(",")[2]));
+				}
+				endTime = System.nanoTime();
+				
+				
+				System.out.println("méthode classique : " + (endTime - startTime) + " ns");
+				System.out.println("---");
+				System.out.println("Taille Map : "+flyweightFactory.getListObjSpa().size());
+				System.out.println("Taille MapBis : "+ listLourde.size());
+				System.out.println("********************");
+		
+	}
 }
--- src/exemple/PoidsMoucheFactory.java
+++ src/exemple/PoidsMoucheFactory.java
@@ -6,42 +6,42 @@ import java.util.Map;
 public class PoidsMoucheFactory {
 
 
-private Map<String,IObjetSpatial> listObjSpa = new HashMap<String,IObjetSpatial>();
+	private Map<String,IObjetSpatial> listObjSpa = new HashMap<String,IObjetSpatial>();
 
 
-public void initFactory(){
-    
-    
-    }
-
- /**
-* Retourne un "PoidsMouche" partagé
-* Si la valeur passé en paramètre
-* correspond à un "PoidsMouche" partagé déjà existant,
-* on le retourne.
-* Sinon on crée une nouvelle instance,
-* on la stocke et on la retourne.
-* @param pValeur Valeur du "PoidsMouche" désiré
-* @return un "PoidsMouche"
-*/
- public IObjetSpatial getObjetSpatial(String pValeur) {
-     
- if(listObjSpa.containsKey(pValeur)) {
- System.out.println("--> Retourne un PoidsMouche (" + pValeur + ") partagé déjà existant");
- return listObjSpa.get(pValeur);
- }
- else {
- System.out.println("--> Retourne un PoidsMouche (" + pValeur +  ") partagé non déjà existant");
- final IObjetSpatial nouveauIObjetSpatial = new ObjetSpacialPartage(pValeur);
- listObjSpa.put(pValeur, nouveauIObjetSpatial);
- 
- return nouveauIObjetSpatial;
- }
- }
+	public void initFactory(){
 
 
-public Map<String,IObjetSpatial> getListObjSpa() {
-    return listObjSpa;
-}
+	}
+
+	/**
+	 * Retourne un "PoidsMouche" partagé
+	 * Si la valeur passé en paramètre
+	 * correspond à un "PoidsMouche" partagé déjà existant,
+	 * on le retourne.
+	 * Sinon on crée une nouvelle instance,
+	 * on la stocke et on la retourne.
+	 * @param pValeur Valeur du "PoidsMouche" désiré
+	 * @return un "PoidsMouche"
+	 */
+	public IObjetSpatial getObjetSpatial(String pValeur) {
+
+		if(listObjSpa.containsKey(pValeur)) {
+			// System.out.println("--> Retourne un PoidsMouche (" + pValeur + ") partagé déjà existant");
+			return listObjSpa.get(pValeur);
+		}
+		else {
+			// System.out.println("--> Retourne un PoidsMouche (" + pValeur +  ") partagé non déjà existant");
+			final IObjetSpatial nouveauIObjetSpatial = new ObjetSpacialPartage(pValeur);
+			listObjSpa.put(pValeur, nouveauIObjetSpatial);
+
+			return nouveauIObjetSpatial;
+		}
+	}
+
+
+	public Map<String,IObjetSpatial> getListObjSpa() {
+		return listObjSpa;
+	}
 
 }
