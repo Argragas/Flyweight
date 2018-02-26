99afa0533880259e02576cfcd7d1c72f4e202d14
Argragas <Argragas@DESKTOP-DU63UOI>
2018-02-26 15:11:59 +0100

    ajout de  commentaires et de fichiers de tests

---
+++ src/exemple/ObjetSpacialNonPartage.java
@@ -0,0 +1,35 @@
+package exemple;
+
+/**
+ * {@link ObjetSpatial} spécifique ne pouvant être partagé.
+ * @author Argragas
+ *
+ */
+public class ObjetSpacialNonPartage extends ObjetSpatial implements IObjetSpatial   {
+
+	private String commentaire;
+
+	
+	public ObjetSpacialNonPartage() {
+		super();
+	}
+
+
+	public ObjetSpacialNonPartage(String pays, String commentaire) {
+		this.commentaire =  commentaire;
+		this.setPays(pays);
+	}
+
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	
+	
+}
